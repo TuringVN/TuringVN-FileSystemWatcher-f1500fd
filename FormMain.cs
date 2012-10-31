a91b4731ea89a7672557c749146fc427873ca279
TuringVN <TuringVN@gmail.com>
2012-10-31 13:31:28 +0700

    2
    commit

--- FormMain.cs
+++ FormMain.cs
@@ -39,7 +39,7 @@ namespace FileChangeNotifier
             {
                 m_bIsWatching = true;
                 btnWatchFile.BackColor = Color.Red;
-                btnWatchFile.Text = "Stop Watching";
+                btnWatchFile.Text = "Stop Watching change";
 
                 m_Watcher = new System.IO.FileSystemWatcher();
                 if (rdbDir.Checked)
